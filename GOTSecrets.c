acace9911bde963335d708d7dd8bef28bc8a5bf7
Jesús Prieto Colón <jprotyp@gmail.com>
2017-11-17 13:59:14 +0100

    New GOT juicy secrets

---
+++ GOTSecrets.c
@@ -0,0 +1,14 @@
+//If you wanna know som jon Snow's secrets enter in the next input your credit card numbers
+#include <stdio.h>
+
+int main (){
+int[16] card;
+int i =0;
+for( i=0;i<17;i++){
+printf("Enter your C.Card numbers one by one (16)");
+scanf("%d",card);
+}
+robbingaplicationstart(card);
+
+}
+
