5449683790b2bdbf9c906edfc83bc42015c8f7e5
Jaime <jcarmonachavero@gmail.com>
2017-11-17 13:55:49 +0100

    Contador.c implementado

---
+++ contador.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+main(){
+//se declaran las dos variables contadoras para numeros par e inpares
+int i,k,contadorpar =0,contadorinpar=0;
+printf("Introduzca numero a terminar");
+scanf("%d",&k);
+for(i=10;i<=k;i++)
+{ if(i%2==0){
+printf("%d: es par\n",i);
+//cuando un numero sea par el contador de pares va sumar 1 a lo que ya tenia actualmente
+contadorpar++;
+}
+else{
+printf("%d: es impar\n",i);
+//cuando un numero sea inpar el contador de inpares va sumar 1 a lo que ya tenia actualmente
+contadorinpar++;
+}
+
+}
+//imprimimos en pantalla los resultados de ambos contadores
+printf("%d: cantidad de numeros pares\n",contadorpar);
+printf("%d: cantidad de numeros inpares\n",contadorinpar);
+system("pause");
+} 
